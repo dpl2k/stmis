08fb626608e0b8e64ecb10ed8c2df6fb96a61181
dpl2k <longhoang732000@gmail.com>
2024-04-14 16:53:08 +0700

    add sticky header for tables

--- src/components/DeliveryCategoryTable/index.jsx
+++ src/components/DeliveryCategoryTable/index.jsx
@@ -16,9 +16,9 @@ const DeliveryCategoryTable = ({ deliveryCategories, onDelete, onEdit }) => {
     };
 
     return (
-        <TableContainer component={Paper}>
+        <TableContainer component={Paper} style={{ overflowX: "initial" }}>
             <Table>
-                <TableHead>
+                <TableHead style={{ position: 'sticky', top: 0, zIndex: 1 }}>
                     <TableRow style={{ backgroundColor: '#3f51b5' }}>
                         <TableCell style={{ fontWeight: '600', color: '#ffffff' }}>Name</TableCell>
                         <TableCell style={{ fontWeight: '600', color: '#ffffff' }}>Parent Category</TableCell>
